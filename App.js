df19ee3cb3c2420b1e8644be8d306e79f96e4fb1
Brian De Maio <bdemaio1@gmail.com>
2020-02-14 17:57:46 -0500

    Splitting out components

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import Header from './components/Header';
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}> Hello World </Text>
+      <Header />
     </View>
   );
 };
@@ -11,12 +12,7 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
+    paddingTop: 60,
   },
 });
 
