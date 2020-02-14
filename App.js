0fa8f5bcf4f65dbb065b47beae6ad60808f08cf7
Brian De Maio <bdemaio1@gmail.com>
2020-02-14 18:32:45 -0500

    A little more error checking when trying to add items

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, StyleSheet, FlatList} from 'react-native';
+import {View, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
 import AddItem from './components/AddItem';
@@ -30,10 +30,20 @@ const App = () => {
     });
   };
 
+  const addItem = text => {
+    if (!text) {
+      Alert.alert('Error', 'Please enter an item', {text: 'Ok'});
+    } else {
+      setItems(prevItems => {
+        return [{id: uuid(), text}, ...prevItems];
+      });
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Header />
-      <AddItem />
+      <AddItem addItem={addItem} />
       <FlatList
         data={items}
         renderItem={({item}) => (
